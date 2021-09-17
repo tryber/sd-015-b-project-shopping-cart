c395594fb30cd3ecb0a0099195924e94a1df3109
findCarolinaCosta <carolinadacosta1997@gmail.com>
2021-09-17 16:07:41 -0300

    Requisito 1

--- script.js
+++ script.js
@@ -20,24 +20,44 @@ function createProductItemElement({ sku, name, image }) {
   section.appendChild(createCustomElement('span', 'item__title', name));
   section.appendChild(createProductImageElement(image));
   section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
+  document.querySelector('.items').appendChild(section);
 
   return section;
 }
 
-function getSkuFromProductItem(item) {
-  return item.querySelector('span.item__sku').innerText;
-}
+// function getSkuFromProductItem(item) {
+//   return item.querySelector('span.item__sku').innerText;
+// }
 
-function cartItemClickListener(event) {
-  // coloque seu código aqui
-}
+// function cartItemClickListener(event) {
+//   // coloque seu código aqui
+// }
 
-function createCartItemElement({ sku, name, salePrice }) {
-  const li = document.createElement('li');
-  li.className = 'cart__item';
-  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
-  return li;
-}
+// function createCartItemElement({ sku, name, salePrice }) {
+//   const li = document.createElement('li');
+//   li.className = 'cart__item';
+//   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+//   li.addEventListener('click', cartItemClickListener);
+//   return li;
+// }
 
-window.onload = () => { };
+const fetchProduct = () => fetch('https://api.mercadolibre.com/sites/MLB/search?q=computador')
+  .then((answer) => answer.json());
+
+const addSectionProduct = (product) => {
+  const productData = createProductItemElement({
+    sku: product.id,
+    name: product.title,
+    image: product.thumbnail,
+  });
+  document.querySelector('.items').appendChild(productData);
+};
+
+window.onload = () => {
+  fetchProduct()
+    .then((productData) => {
+      productData.results.forEach((result) => {
+        addSectionProduct(result);
+      });
+    });
+};
