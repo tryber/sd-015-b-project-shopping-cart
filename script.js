265e8f1dca707a7be9b3421ae5e7dc33ceb2d69d
Antônio Campos <aclcampos@live.com>
2021-09-17 18:58:59 -0300

    Realizado requisito 1

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// func 1 - cria uma "img", insere uma classe "item__image" e atribui um src para buscar a imagem
+// imageSource -> link da imagem
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
@@ -5,6 +7,7 @@ function createProductImageElement(imageSource) {
   return img;
 }
 
+// func 2 - cria um elemento com base no primeiro parâmetro, insere uma classe baseado no segundo parâmetro e insere um texto baseado no terceiro parâmetro
 function createCustomElement(element, className, innerText) {
   const e = document.createElement(element);
   e.className = className;
@@ -12,6 +15,7 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
+// func 3 - Cria uma "section" no html, e insere uma classe chamada "item", depois criam elementos baseados na função "createCustomElement"
 function createProductItemElement({ sku, name, image }) {
   const section = document.createElement('section');
   section.className = 'item';
@@ -24,6 +28,7 @@ function createProductItemElement({ sku, name, image }) {
   return section;
 }
 
+// func 4 - retorna o sku do item dado por parâmetro
 function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
 }
@@ -40,4 +45,18 @@ function createCartItemElement({ sku, name, salePrice }) {
   return li;
 }
 
-window.onload = () => { };
+function getItemsList() {
+  const itemList = document.querySelector('.items');
+  return fetch('https://api.mercadolibre.com/sites/MLB/search?q=computador')
+  .then((response) => response.json())
+  .then((data) => data.results)
+  .then((itens) => itens.forEach((item) => {
+    itemList.appendChild(createProductItemElement(
+      { sku: item.id, name: item.title, image: item.thumbnail },
+));
+  }));
+}
+
+window.onload = () => { 
+  getItemsList();
+};
