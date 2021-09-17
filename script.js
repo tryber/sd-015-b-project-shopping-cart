474fbfa377729ba2527c63afcb4dc50b68ddca48
Arthur <viegas.arthur2013@hotmail.com>
2021-09-17 18:07:12 -0300

    requisito 1

--- script.js
+++ script.js
@@ -15,14 +15,30 @@ function createCustomElement(element, className, innerText) {
 function createProductItemElement({ sku, name, image }) {
   const section = document.createElement('section');
   section.className = 'item';
-
+  
   section.appendChild(createCustomElement('span', 'item__sku', sku));
   section.appendChild(createCustomElement('span', 'item__title', name));
   section.appendChild(createProductImageElement(image));
   section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
-
+  
   return section;
 }
+const findItems = (product) => {
+const API_LINK = `https://api.mercadolibre.com/sites/MLB/search?q=${product}`;
+  fetch(API_LINK)
+  .then((response) => response.json())
+  .then((obj) => obj.results)
+  .then((products) => products.forEach((item) => {
+    const items = document.querySelector('.items');
+    items.appendChild(createProductItemElement(
+      { sku: item.id, name: item.title, image: item.thumbnail },
+    ));
+  }))
+  .catch((erro) => console
+  .error(`${erro}: erro na função findItems, possivelmente no link da API`));
+};
+
+// console.log(findItem());
 
 function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
@@ -40,4 +56,6 @@ function createCartItemElement({ sku, name, salePrice }) {
   return li;
 }
 
-window.onload = () => { };
+window.onload = () => {
+  findItems('computador');
+};
