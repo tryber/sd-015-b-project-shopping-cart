cfb251cd49dc255601639b07bba6ada83f571ee4
anniehau <anniehaurani@gmail.com>
2021-09-17 15:47:25 -0300

    Requisito 1 completo.

--- script.js
+++ script.js
@@ -15,7 +15,6 @@ function createCustomElement(element, className, innerText) {
 function createProductItemElement({ sku, name, image }) {
   const section = document.createElement('section');
   section.className = 'item';
-
   section.appendChild(createCustomElement('span', 'item__sku', sku));
   section.appendChild(createCustomElement('span', 'item__title', name));
   section.appendChild(createProductImageElement(image));
@@ -40,4 +39,19 @@ function createCartItemElement({ sku, name, salePrice }) {
   return li;
 }
 
-window.onload = () => { };
+function fetchProducts() {
+  fetch('https://api.mercadolibre.com/sites/MLB/search?q=computador')
+    .then((data) => data.json())
+    .then((product) => {
+      product.results.forEach(({id, title, thumbnail}) => {
+        const item = { sku: id, name: title, image: thumbnail };
+        const productList = document.querySelector('section .items');
+        const productToList = createProductItemElement(item);
+        productList.appendChild(productToList);
+      })
+    })
+}
+
+window.onload = () => {
+  fetchProducts();
+}
