6c07dd3c69448e3dc48a8dcaec2f91555f70731f
matheusg18 <matheusgabriel.18@gmail.com>
2021-09-17 18:12:42 -0300

    Requisito 2 feito :heavy_check_mark:

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-const URL = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
+const PC_SEARCH_URL = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
+const ITEM_BASE_URL = 'https://api.mercadolibre.com/items/';
 
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
@@ -12,18 +13,6 @@ function createCustomElement(element, className, innerText) {
   e.className = className;
   e.innerText = innerText;
   return e;
-}
-
-function createProductItemElement({ sku, name, image }) {
-  const section = document.createElement('section');
-  section.className = 'item';
-
-  section.appendChild(createCustomElement('span', 'item__sku', sku));
-  section.appendChild(createCustomElement('span', 'item__title', name));
-  section.appendChild(createProductImageElement(image));
-  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
-
-  return section;
 }
 
 function getSkuFromProductItem(item) {
@@ -42,6 +31,34 @@ function createCartItemElement({ sku, name, salePrice }) {
   return li;
 }
 
+function handleAddButton(event) {
+  const itemContainer = event.target.parentElement;
+  const sku = getSkuFromProductItem(itemContainer);
+
+  fetch(ITEM_BASE_URL + sku)
+    .then((response) => response.json())
+    .then(({ id, title, price }) => {
+      const cartItemsOl = document.querySelector('.cart__items');
+      const product = { sku: id, name: title, salePrice: price };
+      cartItemsOl.appendChild(createCartItemElement(product));
+    });
+}
+
+function createProductItemElement({ sku, name, image }) {
+  const section = document.createElement('section');
+  const button = createCustomElement('button', 'item__add', 'Adicionar ao carrinho!');
+  section.className = 'item';
+
+  section.appendChild(createCustomElement('span', 'item__sku', sku));
+  section.appendChild(createCustomElement('span', 'item__title', name));
+  section.appendChild(createProductImageElement(image));
+
+  section.appendChild(button);
+  button.addEventListener('click', handleAddButton);
+
+  return section;
+}
+
 function fillItemsSection(products) {
   const itemsSection = document.querySelector('.items');
   products.forEach(({ id, title, thumbnail }) =>
@@ -49,7 +66,7 @@ function fillItemsSection(products) {
 }
 
 function fetchMercadoLivre() {
-  fetch(URL)
+  fetch(PC_SEARCH_URL)
     .then((response) => response.json())
     .then(({ results: products }) => fillItemsSection(products));
 }
