80942cec91fe01f151c8d2b390d367cd92ab144f
Alex Menezes <alexmenezes.business@outlook.com>
2021-09-17 16:43:59 -0300

    adiciona requisito 1

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+const Query = 'computador';
+const API_URL = `https://api.mercadolibre.com/sites/MLB/search?q=${Query}`;
+
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
@@ -14,14 +17,24 @@ function createCustomElement(element, className, innerText) {
 function createProductItemElement({ sku, name, image }) {
   const section = document.createElement('section');
   section.className = 'item';
-
+  
   section.appendChild(createCustomElement('span', 'item__sku', sku));
   section.appendChild(createCustomElement('span', 'item__title', name));
   section.appendChild(createProductImageElement(image));
   section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
-
+  
   return section;
 }
+
+fetch(API_URL)
+.then((response) => response.json())
+.then((data) => data.results)
+.then((results) => results.forEach((computer) => {
+  const items = document.querySelector('.items');
+   items.appendChild(createProductItemElement(
+     { sku: computer.id, name: computer.title, image: computer.thumbnail },
+     ));
+ }));
 
 function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
