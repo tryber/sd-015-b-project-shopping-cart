ded19fe2765863fb35637677be7711685ba9568c
Caio R. Vieira <caio.robs98@gmail.com>
2021-09-17 15:45:13 -0300

    refatora todas as funçoes para arrow function e implementa a funçao getItemsList que busca pela lista de computadores

--- script.js
+++ script.js
@@ -1,18 +1,27 @@
-function createProductImageElement(imageSource) {
+const wantedProduct = 'computador';
+const API_URL = `https://api.mercadolibre.com/sites/MLB/search?q=$${wantedProduct}`;
+
+const getProductsList = async () => {
+  const data = await fetch(API_URL);
+  const result = await data.json();
+  return result.results;
+};
+
+const createProductImageElement = (imageSource) => {
   const img = document.createElement('img');
   img.className = 'item__image';
   img.src = imageSource;
   return img;
-}
+};
 
-function createCustomElement(element, className, innerText) {
+const createCustomElement = (element, className, innerText) => {
   const e = document.createElement(element);
   e.className = className;
   e.innerText = innerText;
   return e;
-}
+};
 
-function createProductItemElement({ sku, name, image }) {
+const createProductItemElement = ({ sku, name, image }) => {
   const section = document.createElement('section');
   section.className = 'item';
 
@@ -22,22 +31,25 @@ function createProductItemElement({ sku, name, image }) {
   section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
 
   return section;
-}
+};
 
-function getSkuFromProductItem(item) {
+const getSkuFromProductItem = (item) => {
   return item.querySelector('span.item__sku').innerText;
-}
+};
 
-function cartItemClickListener(event) {
+const cartItemClickListener = (event) => {
   // coloque seu código aqui
-}
+};
 
-function createCartItemElement({ sku, name, salePrice }) {
+const createCartItemElement = ({ sku, name, salePrice }) => {
   const li = document.createElement('li');
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
   li.addEventListener('click', cartItemClickListener);
   return li;
-}
+};
 
-window.onload = () => { };
+window.onload = async () => {
+  const list = await getProductsList();
+  console.log(list);
+};
