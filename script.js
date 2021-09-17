95b2092158bfb99cd0910c40327316b6b1872738
Nicolas Franzolin <nicolas.franzolin@gmail.com>
2021-09-17 18:36:57 -0300

    Adiciona Requisito 1

--- script.js
+++ script.js
@@ -12,14 +12,16 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
-function createProductItemElement({ sku, name, image }) {
+function createProductItemElement({ id: sku, title: name, thumbnail: image }) {
   const section = document.createElement('section');
   section.className = 'item';
 
   section.appendChild(createCustomElement('span', 'item__sku', sku));
   section.appendChild(createCustomElement('span', 'item__title', name));
   section.appendChild(createProductImageElement(image));
-  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
+  section.appendChild(
+    createCustomElement('button', 'item__add', 'Adicionar ao carrinho!')
+  );
 
   return section;
 }
@@ -40,4 +42,22 @@ function createCartItemElement({ sku, name, salePrice }) {
   return li;
 }
 
-window.onload = () => { };
+const API_URL_MLCOMPUTER = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
+
+async function requestMLComputer(url) {
+  try {
+    return fetch(url)
+      .then((response) => response.json())
+      .then((data) => data.results.map(({ id, title, thumbnail }) => {
+          const objt = { id, title, thumbnail };
+          const sectionItems = document.querySelector('.items'); // pegando o items pra poder jogar o objt como filho dele.
+          const eachComputer = createProductItemElement(objt); // usando a função previamente criada para fazer um produto. 
+          return sectionItems.append(eachComputer); // colocando cada produto criado na seção de items.
+        }));        
+  } catch (error) {
+    console.log(error);
+  }
+}
+window.onload = () => {
+  requestMLComputer(API_URL_MLCOMPUTER);
+};
