9f06fdff542bd5906265631e2e0bec964f4ea0d1
Rodrigo Lima <rodrigootavio19@gmail.com>
2021-09-17 15:48:01 -0400

    cumpre requisito 3

--- script.js
+++ script.js
@@ -35,6 +35,7 @@ function createProductItemElement({ sku, name, image }) {
 
 function cartItemClickListener(event) {
   // coloque seu código aqui
+  event.target.remove();
 }
 
 function createCartItemElement({ sku, name, salePrice }) {
