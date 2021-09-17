c20637a485c3cdbde51f76caafcf037e9848fe82
LeonardoMonti <contatoleomonti@gmail.com>
2021-09-17 19:09:27 -0300

    requisito 3

--- script.js
+++ script.js
@@ -29,7 +29,8 @@ function getSkuFromProductItem(item) {
 }
 
 function cartItemClickListener(event) {
-  // coloque seu código aqui
+  const eventoClick = event.target;
+  eventoClick.remove();
 }
 
 function createCartItemElement({ sku, name, salePrice }) {
