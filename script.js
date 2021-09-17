738c5cb448543fe87ba26c19277760c292b7de0d
aaguero96 <andre.luiz_1996@hotmail.com>
2021-09-17 19:06:26 -0300

    Finish part 1

--- script.js
+++ script.js
@@ -15,29 +15,43 @@ function createCustomElement(element, className, innerText) {
 function createProductItemElement({ sku, name, image }) {
   const section = document.createElement('section');
   section.className = 'item';
-
   section.appendChild(createCustomElement('span', 'item__sku', sku));
   section.appendChild(createCustomElement('span', 'item__title', name));
   section.appendChild(createProductImageElement(image));
   section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
-
   return section;
 }
 
-function getSkuFromProductItem(item) {
-  return item.querySelector('span.item__sku').innerText;
-}
+// function getSkuFromProductItem(item) {
+//   return item.querySelector('span.item__sku').innerText;
+// }
 
-function cartItemClickListener(event) {
-  // coloque seu código aqui
-}
+// function cartItemClickListener(event) {
+//   // coloque seu código aqui
+// }
 
-function createCartItemElement({ sku, name, salePrice }) {
-  const li = document.createElement('li');
-  li.className = 'cart__item';
-  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
-  return li;
-}
+// function createCartItemElement({ sku, name, salePrice }) {
+//   const li = document.createElement('li');
+//   li.className = 'cart__item';
+//   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+//   li.addEventListener('click', cartItemClickListener);
+//   return li;
+// }
 
-window.onload = () => { };
+const computersArrayPromise = new Promise((resolve, reject) => {
+  fetch('https://api.mercadolibre.com/sites/MLB/search?q=computador')
+    .then((result) => result.json())
+    .then((resultJson) => resolve(resultJson.results));
+});
+
+window.onload = () => {
+  const sectionItemsElement = document.querySelector('section .items');
+  computersArrayPromise
+    .then((results) => {
+      results.forEach((element) => {
+        const { id: sku, title: name, thumbnail: image } = element;
+        const newElement = createProductItemElement({ sku, name, image });
+        sectionItemsElement.appendChild(newElement);
+      });
+    });
+};
