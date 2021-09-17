e0863c5719b2f71b4ba9d503f045f084a9270797
Ericksonn Siqueira <ericksonsiqueirasilva@gmail.com>
2021-09-17 16:22:46 -0300

    Requisito 1, gerando lista de items de acordo com busca

--- script.js
+++ script.js
@@ -40,4 +40,32 @@ function createCartItemElement({ sku, name, salePrice }) {
   return li;
 }
 
-window.onload = () => { };
+async function getProductList(productName) {
+  const API_URL = `https://api.mercadolibre.com/sites/MLB/search?q=${productName}`;
+  const productListPromise = await fetch(API_URL);
+  const productListConverted = await productListPromise.json();
+
+  return productListConverted;
+}
+
+function makeObjForProductItem(product) {
+  const { id, title, thumbnail } = product;
+  const productObj = { sku: id, name: title, image: thumbnail };
+  return productObj;
+}
+
+async function generateProductList(prodctName) {
+  const productList = await getProductList(prodctName);
+  const prodctListResults = productList.results;
+  const itemsSections = document.querySelector('.items');
+
+  prodctListResults.forEach((product) => {
+    const objForProductItem = makeObjForProductItem(product);
+    const productItemElement = createProductItemElement(objForProductItem);
+    itemsSections.appendChild(productItemElement);
+  });
+}
+
+window.onload = () => {
+  generateProductList('computador');
+};
