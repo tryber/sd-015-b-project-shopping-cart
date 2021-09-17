36ef601e699369967305e9a8fe680c58ca557fd0
Antonio Souza <antoniofelipe2001@hotmail.com>
2021-09-17 18:31:21 -0300

    1° Ajuste de lint

--- script.js
+++ script.js
@@ -1,3 +1,59 @@
+const cartElement = document.querySelector('ol.cart__items');
+
+function addElementToParent(element, parentSelector) {
+  const parentElment = document.querySelector(parentSelector);
+  parentElment.appendChild(element);
+}
+
+function appendLoadingElement() {
+  const loadingElement = document.createElement('p');
+  loadingElement.className = 'loading';
+  loadingElement.innerText = 'Loading...';
+  addElementToParent(loadingElement, 'section.items');
+}
+
+function removeLoadingElement() {
+  const loadingElement = document.querySelector('.loading');
+  loadingElement.remove();
+}
+
+function saveCartOnLocalStorage() {
+  const cartHtml = cartElement.innerHTML;
+  localStorage.setItem('cart', cartHtml);
+}
+
+function apeendCartTotal(total) {
+  const cartTotalElement = document.querySelector('div.total-price');
+  cartTotalElement.innerText = `${total}`;
+}
+
+function calculateCartTotal() {
+  const cartItemsCollection = cartElement.children;
+  const cartItemsList = [...cartItemsCollection];
+  const total = cartItemsList.reduce((acc, act) => {
+    const texto = act.innerText;
+    const priceIndex = texto.match(/(PRICE: \W)/).index + 8;
+    const price = Number(texto.slice(priceIndex));
+    return acc + price;
+  }, 0);
+  apeendCartTotal(total);
+}
+
+function cartItemClickListener(event) {
+  const element = event.target;
+  element.remove();
+  calculateCartTotal();
+  saveCartOnLocalStorage();
+}
+
+function loadCartFromLocalStorage() {
+  const savedCart = localStorage.getItem('cart');
+  cartElement.innerHTML = savedCart;
+  const cartItemsCollection = cartElement.children;
+  const cartItemsList = [...cartItemsCollection];
+  cartItemsList.forEach((item) => item.addEventListener('click', cartItemClickListener));
+}
+
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
@@ -24,56 +80,12 @@ function createProductItemElement({ sku, name, image }) {
   return section;
 }
 
-function getSkuFromProductItem(item) {
-  return item.querySelector('span.item__sku').innerText;
-}
-
-function apeendCartTotal(total) {
-  const cartTotalElement = document.querySelector('div.total-price');
-  cartTotalElement.innerText = `${total}`;
-}
-
-function calculateCartTotal() {
-  const cartElement = document.querySelector('ol.cart__items');
-  const cartItemsCollection = cartElement.children;
-  const cartItemsList = [...cartItemsCollection];
-  const total = cartItemsList.reduce((acc, act) => {
-    const texto = act.innerText;
-    const priceIndex = texto.match(/(PRICE: \W)/).index + 8
-    const price = Number(texto.slice(priceIndex));
-    return acc + price;
-  }, 0)
-  apeendCartTotal(total);
-}
-
-function cartItemClickListener(event) {
-  const element = event.target;
-  element.remove();
-  calculateCartTotal();
-  saveCartOnLocalStorage();
-}
-
 function createCartItemElement({ sku, name, salePrice }) {
   const li = document.createElement('li');
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
   li.addEventListener('click', cartItemClickListener);
   return li;
-}
-
-function saveCartOnLocalStorage() {
-  const cart = document.querySelector('ol.cart__items');
-  const cartHtml = cart.innerHTML;
-  localStorage.setItem('cart', cartHtml)
-}
-
-function loadCartFromLocalStorage() {
-  const savedCart = localStorage.getItem('cart');
-  const actualCart = document.querySelector('ol.cart__items');
-  actualCart.innerHTML = savedCart;
-  const cartItemsCollection = actualCart.children;
-  const cartItemsList = [...cartItemsCollection];
-  cartItemsList.forEach((item) => item.addEventListener('click', cartItemClickListener));
 }
 
 async function getProductById(id) {
@@ -90,11 +102,6 @@ async function getProductsList() {
   const jsonResponse = await reponse.json();
   const resultsList = await jsonResponse.results;
   return resultsList;
-}
-
-function addElementToParent(element, parentSelector) {
-  const parentElment = document.querySelector(parentSelector);
-  parentElment.appendChild(element);
 }
 
 async function appendProductList() {
@@ -121,7 +128,6 @@ async function addToCart(event) {
 }
 
 function emptyCart() {
-  const cartElement = document.querySelector('ol.cart__items')
   cartElement.innerHTML = '';
   localStorage.removeItem('cart');
   calculateCartTotal();
@@ -132,19 +138,7 @@ function addEventListeners() {
   const addToCartButtomsList = [...addToCartButtomsColletction];
   addToCartButtomsList.forEach((buttom) => buttom.addEventListener('click', addToCart));
   const emptyCartButtom = document.querySelector('.empty-cart');
-  emptyCartButtom.addEventListener('click', emptyCart)
-}
-
-function appendLoadingElement() {
-  const loadingElement = document.createElement('p');
-  loadingElement.className = 'loading';
-  loadingElement.innerText = 'Loading...';
-  addElementToParent(loadingElement, 'section.items');
-}
-
-function removeLoadingElement() {
-  const loadingElement = document.querySelector('.loading');
-  loadingElement.remove();
+  emptyCartButtom.addEventListener('click', emptyCart);
 }
 
 window.onload = async () => {
--- tests.js
+++
@@ -1,6 +0,0 @@
-const string = `SKU: MLB1937079157 | NAME: Pc Computador Cpu Core I5 650 + Ssd 240gb, 8gb Memória Ram 
-| PRICE: $1419.85`
-
-const number = string.match(/(PRICE: \W)/).index + 8
-
-console.log(Number(string.slice(number)))
