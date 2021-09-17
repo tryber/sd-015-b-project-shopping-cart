2cfb78c43e711f6e5304f5cbf72f719cbb481433
MarcoMecenasFilho <marcomecenasfilho@gmail.com>
2021-09-17 20:32:26 -0300

    requesito 1 e 2

--- script.js
+++ script.js
@@ -6,13 +6,13 @@ function createProductImageElement(imageSource) {
 }
 
 function createCustomElement(element, className, innerText) {
-  const e = document.createElement(element);
-  e.className = className;
-  e.innerText = innerText;
-  return e;
+  const event = document.createElement(element);
+  event.className = className;
+  event.innerText = innerText;
+  return event;
 }
 
-function createProductItemElement({ sku, name, image }) {
+function createProductItemElement({ id: sku, title: name, thumbnail: image }) {
   const section = document.createElement('section');
   section.className = 'item';
 
@@ -29,10 +29,10 @@ function getSkuFromProductItem(item) {
 }
 
 function cartItemClickListener(event) {
-  // coloque seu código aqui
+  event.target.remove();
 }
 
-function createCartItemElement({ sku, name, salePrice }) {
+function createCartItemElement({ id: sku, title: name, price: salePrice }) {
   const li = document.createElement('li');
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
@@ -40,4 +40,54 @@ function createCartItemElement({ sku, name, salePrice }) {
   return li;
 }
 
-window.onload = () => { };
+//  utilizei parte dos codigos utilizados na aula 9.2
+// https://github.com/tryber/sd-015-b-live-lectures/pull/23
+const requestComputers = async () => {
+ const computerResults = await fetch('https://api.mercadolibre.com/sites/MLB/search?q=computador');
+ const computerList = await computerResults.json();
+    computerList.results.forEach(({ id, title, thumbnail }) => {
+    const productList = document.querySelector('section .items');
+    const listProduct = createProductItemElement({ id, title, thumbnail });
+    productList.appendChild(listProduct);
+    });
+    //  Depois de obter o json, pegamos o array results que ele possui event 
+    // fazemos um forEach para que cada elemento seja aplicado.Podem passamos como
+    // parametro o id, title event thumbnail, lembrando que eles fora "traduzidos" na função
+    // creatProductItemElement. trocamos o nome da chave do objeto., depois disso 
+    // procuramos onde esse elemento sera criado, semdo filho da section com a classe .item,
+    // event adicionamos  esse filho utilizando a função.
+};
+// requesito 2
+// acessado: https://github.com/tryber/sd-014-b-project-shopping-cart/blob/antonio-martins-neto-shopping-cart/script.js
+const getIdProductSelected = async (id) => {
+  const idResult = await fetch(`https://api.mercadolibre.com/items/${id}`);
+  const idJson = await idResult.json();
+  return idJson;
+};
+// Aquí procuramos outras informações por outro endpoint. E retornamos um
+// json,
+
+const addToCart = () => {
+  const section = document.querySelector('.items');
+  // Aquí buscamos os elementos com a classe .items
+  section.addEventListener('click', async (event) => {
+    //  Aquí add um "escutador" quando damos um click, neses elementos que possue a classe .items, ele gera uma função assincrona;
+    if (event.target.className === 'item__add') {
+      // Aqui fizemos uma condição se o evento do click "target", possuir a classe .item__add ele continua.
+      const cartItens = document.querySelector('.cart__items');
+      const itemParent = event.target.parentElement;
+      // Aqui buscamos o pai do alvo do click, já que o alvo foi um button precisamos do elemento que ele está inserido, nesse caso o item
+      const itemId = getSkuFromProductItem(itemParent);
+      // Aqui pegamos esse elemento pai, e jogamos na função para obter seu id:sku,
+      const itemDetails = await getIdProductSelected(itemId);
+      // Pegmos sku obtido e jogamos na função para obter as informações com o id obtido
+      cartItens.appendChild(createCartItemElement(itemDetails));
+      // Aqui criamos esse item clicado no carrinho de compras;
+    }
+  });
+};
+
+window.onload = () => { 
+  requestComputers();
+  addToCart();
+};
