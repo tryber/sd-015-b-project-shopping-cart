8f0e6b1e7403d1d52f9723a61d0441251fa27845
EzequielVerissimoCarvalhoZica <ezeverissimo@gmail.com>
2021-09-18 10:40:04 -0300

    Adiciona requisito 2 e 3

--- script.js
+++ script.js
@@ -14,11 +14,11 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
-function createProductItemElement({ id, name, image }) {
+function createProductItemElement({ sku, name, image }) {
   const section = document.createElement('section');
   section.className = 'item';
   
-  section.appendChild(createCustomElement('span', 'item__sku', id));
+  section.appendChild(createCustomElement('span', 'item__sku', sku));
   section.appendChild(createCustomElement('span', 'item__title', name));
   section.appendChild(createProductImageElement(image));
   section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
@@ -39,26 +39,50 @@ function addProducts(products) {
   });
 }
 
+function cartItemClickListener(event) {
+  const buttonAdd = document.querySelector('.item_add');
+  event.target.remove();
+}
+
 function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
 }
 
-function cartItemClickListener(event) {
-  // coloque seu código aqui
-}
-
-function createCartItemElement({ id, name, salePrice }) {
+function createCartItemElement({ sku, name, salePrice }) {
   const li = document.createElement('li');
   li.className = 'cart__item';
-  li.innerText = `SKU: ${id} | NAME: ${name} | PRICE: $${salePrice}`;
+  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
   li.addEventListener('click', cartItemClickListener);
   return li;
+}
+
+async function addIdComputer(actualId) {
+  const idComputer = getSkuFromProductItem(actualId);
+  const API_ID = `https://api.mercadolibre.com/items/${idComputer}`;
+  
+  const data = await fetch(API_ID);
+  const computer = await data.json();
+  const { id, title, price } = computer;
+  const dataComputer = {
+    sku: id,
+    name: title,
+    salePrice: price,
+  };
+  const olCart = document.querySelector('.cart__items');
+  olCart.appendChild(createCartItemElement(dataComputer));
+}
+function addEventButtons() {
+  const items = document.querySelectorAll('.item');
+  items.forEach((item) => item.lastChild.addEventListener('click', (() => {
+    addIdComputer(item);
+  })));
 }
 
 window.onload = async () => {
   try {
     const product = await requestComputador();
     addProducts(product);
+    await addEventButtons();
   } catch (error) {
     throw new Error(error);
   }
