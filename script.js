aaa21fd3484af5feffdd7573ba9dfd9629820366
Richard Leal <rwsleal@gmail.com>
2021-09-18 11:48:09 -0300

    Requisito 3 finalizado

--- script.js
+++ script.js
@@ -29,7 +29,8 @@ function getSkuFromProductItem(item) {
 }
 
 function cartItemClickListener(event) {
-  // coloque seu código aqui
+  const cartItem = event.target;
+  cartItem.remove();
 }
 
 function createCartItemElement({ sku, name, salePrice }) {
