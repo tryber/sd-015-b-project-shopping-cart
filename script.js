35eca6cf5b13c6a03102d23256e814344874be4e
Rodrigo Lima <rodrigootavio19@gmail.com>
2021-09-18 14:34:54 -0400

    cumpre requisito 4

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const requisito1 = 'https://api.mercadolibre.com/sites/MLB/search?q=';
-const requisito2 = 'https://api.mercadolibre.com/items/';
+const searchGroupProducts = 'https://api.mercadolibre.com/sites/MLB/search?q=';
+const searchSpecificProduct = 'https://api.mercadolibre.com/items/';
 
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
@@ -29,13 +29,29 @@ function createProductItemElement({ sku, name, image }) {
   return section;
 }
 
-// function getSkuFromProductItem(item) {
-//   return item.querySelector('span.item__sku').innerText;
-// }
+function getSkuFromProductItem(item) {
+  return item.querySelector('span.item__sku').innerText;
+}
+
+function sendCartToCloud() {
+  const cartIems = document.querySelectorAll('.cart__items li');
+  const infoItems = [];
+  cartIems.forEach(({ innerText }) => {
+    const divideByBar = innerText.split('|');
+    const [notTreatedId] = divideByBar;
+    const divideByColon = notTreatedId.split(':');
+    const treatedId = divideByColon[1].trim();
+    infoItems.push(treatedId);
+  });
+  const stringItems = JSON.stringify(infoItems);
+  
+  localStorage.setItem('cart-items-ids', stringItems);
+}
 
 function cartItemClickListener(event) {
   // coloque seu código aqui
   event.target.remove();
+  sendCartToCloud();
 }
 
 function createCartItemElement({ sku, name, salePrice }) {
@@ -55,14 +71,31 @@ async function getItemsFromApi(api, product) {
 
 async function sendToCart(event) {
   const productContainer = event.target.parentNode;
-  const textId = productContainer.querySelector('.item__sku').innerText;
+  const textId = getSkuFromProductItem(productContainer);
   const cartContainer = document.querySelector('.cart__items');
-  const itemDetails = await getItemsFromApi(requisito2, textId);
+  const itemDetails = await getItemsFromApi(searchSpecificProduct, textId);
   const { id, title, price } = itemDetails;
   const searchResult = { sku: id, name: title, salePrice: price };
   const cartItem = createCartItemElement(searchResult);
   
   cartContainer.appendChild(cartItem);
+  sendCartToCloud();
+}
+
+function loadCloudCart() {
+  const stringCloudCart = localStorage.getItem('cart-items-ids');
+  if (stringCloudCart) {
+    const cartContainer = document.querySelector('.cart__items');
+    const cloudCartIds = JSON.parse(stringCloudCart);
+
+    cloudCartIds.forEach(async (productId) => {
+      const itemDetails = await getItemsFromApi(searchSpecificProduct, productId);
+      const { id, title, price } = itemDetails;
+      const searchResult = { sku: id, name: title, salePrice: price };
+      const cartItem = createCartItemElement(searchResult);
+      cartContainer.append(cartItem);
+    });
+  }
 }
 
 function getProductsButtons() {
@@ -75,7 +108,7 @@ function getProductsButtons() {
 
 async function appendListItems() {
   const container = document.querySelector('.items');
-  const research = await getItemsFromApi(requisito1, 'computador');
+  const research = await getItemsFromApi(searchGroupProducts, 'computador');
   
   research.results.forEach(({ id, title, thumbnail }) => {
     const productInfo = { sku: id, name: title, image: thumbnail };
@@ -88,4 +121,5 @@ async function appendListItems() {
 
 window.onload = () => {
   appendListItems();
+  loadCloudCart();
 };
