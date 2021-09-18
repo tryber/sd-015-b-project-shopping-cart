d47ea3bdb8fec8df39a6def97dde87218181e2f5
Camilla Del Guerso <camilladelguerso@gmail.com>
2021-09-17 22:33:43 -0300

    Adiciona requisitos 1, 2, 3 e 4

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+const API_URL = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
+const teste = ('.cart__items');
+
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
@@ -28,8 +31,28 @@ function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
 }
 
+const getApi = () => 
+  fetch(API_URL)
+  .then((response) => response.json())
+  .then((results) => results.results.forEach(({ id, title, thumbnail }) => {
+    const item = { 
+      sku: id, 
+      name: title, 
+      image: thumbnail, 
+    };
+    const getItems = document.querySelector('.items');
+    const itemElement = createProductItemElement(item);
+    getItems.appendChild(itemElement);
+  }));
+
+const saveItemLocal = () => {
+  const getOl = document.querySelectorAll(teste);
+  getOl.forEach((elemento) => localStorage.setItem('key', JSON.stringify(elemento.innerHTML)));
+};
+
 function cartItemClickListener(event) {
-  // coloque seu código aqui
+  event.target.remove();
+  saveItemLocal();
 }
 
 function createCartItemElement({ sku, name, salePrice }) {
@@ -40,4 +63,42 @@ function createCartItemElement({ sku, name, salePrice }) {
   return li;
 }
 
-window.onload = () => { };
+const getItemLocal = () => {
+  const getOl = document.querySelectorAll(teste);
+  getOl.forEach((elemento) => {
+    const ele = elemento;
+    ele.innerHTML = JSON.parse(localStorage.getItem('key'));
+    ele.addEventListener('click', cartItemClickListener);
+  });
+};
+
+const getId = (idItem) => {
+  const valueItem = getSkuFromProductItem(idItem);
+  const API_ID = `https://api.mercadolibre.com/items/${valueItem}`;
+  fetch(API_ID)
+  .then((response) => response.json())
+  .then(({ id, title, price }) => {
+    const priceItem = {
+      sku: id,
+      name: title,
+      salePrice: price,
+    };
+  const getLi = createCartItemElement(priceItem);
+  const getOl = document.querySelector(teste);
+  getOl.appendChild(getLi);
+  saveItemLocal();
+  })
+  .catch(() => console.log('erro na função getId'));
+};
+
+const button = () => {
+  const getSections = document.querySelectorAll('.item');
+  getSections.forEach((elemento) => 
+  elemento.lastChild.addEventListener('click', () => getId(elemento)));
+};
+
+window.onload = () => {
+  getApi()
+  .then(() => button());
+  getItemLocal();
+ };
