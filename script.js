f3562536c35c1fa9217f1292e742ce26a0a5b344
Denis Jonathan <denisjonathan7@gmail.com>
2021-09-19 14:34:40 -0300

    🔧 Alterado como o valor no botão é mostrado

--- script.js
+++ script.js
@@ -21,7 +21,8 @@ function createProductItemElement({ sku, name, price, image }) {
   section.appendChild(createCustomElement('span', 'item__sku', sku));
   section.appendChild(createCustomElement('span', 'item__title', name));
   section.appendChild(createProductImageElement(image));
-  section.appendChild(createCustomElement('button', 'item__add', `R$${price}`));
+  section.appendChild(createCustomElement('button', 'item__add',
+    `${price.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}`));
 
   return section;
 }
