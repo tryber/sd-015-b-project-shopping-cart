c0e9ed8c6b145a8eeee835a6fcb30fb226742d44
Gabrielle-Murat <belle.murat@gmail.com>
2021-09-20 12:23:09 -0300

    Adiciona o requisito 3

--- script.js
+++ script.js
@@ -24,11 +24,17 @@ function createProductItemElement({ sku, name, image }) {
   return section;
 }
 
+function cartItemClickListener(event) {
+  const cartItem = event.target;
+  const cart = cartItem.parentElement;
+  cart.removeChild(cartItem);
+}
+
 function createCartItemElement({ sku, name, salePrice }) {
   const li = document.createElement('li');
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  // li.addEventListener('click', cartItemClickListener);
+  li.addEventListener('click', cartItemClickListener);
   return li;
 }
 
@@ -69,13 +75,9 @@ async function getProductsFromEndpoint() {
   await handleItemsListButtonEventAdd();
 }
 
-function getSkuFromProductItem(item) {
-  return item.querySelector('span.item__sku').innerText;
-}
-
-function cartItemClickListener(event) {
-  // coloque seu código aqui
-}
+// function getSkuFromProductItem(item) {
+//   return item.querySelector('span.item__sku').innerText;
+// }
 
 // function handleAll() {
 //   getProductsFromEndpoint();
