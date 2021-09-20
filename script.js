c7f93e302e22f13927d68725eec3d39998e252de
Danielen Cestari Nunes <danielen.cestari@gmail.com>
2021-09-20 15:38:56 -0300

    requisito 02 com a ajuda do santo leandro

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
-const URL_API = 'https://api.mercadolibre.com/sites/MLB';
+const urlApi = 'https://api.mercadolibre.com/sites/MLB';
+
+// elementos do HTML que não rodam pq o script no index é chamado antes do body.
+//const items = document.querySelector('.items');
+//const cart__items = document.querySelector('.cart__items');
+//const totalPrice = document.querySelector('.total-price');
+//const emptyCart= document.querySelector('.empty-cart');
+//const loading = document.querySelector('.loading');
 
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
@@ -13,28 +20,42 @@ function createCustomElement(element, className, innerText) {
   e.innerText = innerText;
   return e;
 }
+
 async function getComputer() {
-  const response = await fetch(`${URL_API}/search?q=computador`);
+  const response = await fetch(`${urlApi}/search?q=computador`);
   const results = await response.json(); // requisito 01 A lista de produtos que devem ser exibidos é o array results no JSON acima.
-  //console.log(results)
+  //console.log(results) // só pra ver como retorna 
   const listComp = await results.results;
-  //console.log(listComp)
+  //console.log(listComp) // retorna lista de objetos 
   for (const key in listComp) { // for in usa a chave do objeto como indice no percorrimento 
-    //console.log(listComp[key])
-    document.querySelector('.items').appendChild(createProductItemElement(listComp[key]));
+    //console.log(listComp[key]) // pra ver como ele separa cada objeto em um elemento, que será percorrido com o indice chave
+    document.querySelector('.items').appendChild(createProductItemElement(listComp[key])); // faz o apend qdo a função é executada, fazendo assim a lista de produtos.
   }
+}
+/* REQUISITO 2 -  Cada produto na página HTML possui um botão com o nome Adicionar ao carrinho!. Ao clicar nesse botão você deve realizar uma requisição para o endpoint: "https://api.mercadolibre.com/items/$ItemID" onde $ItemID deve ser o valor id do item selecionado.*/
+async function addCarrinho(sku){
+  const response = await fetch(`https://api.mercadolibre.com/items/${sku}`);
+  const results = await response.json(); // pega a resposta da função e transforma em Json.
+  // console.log(results) // só pra ver como retorna 
+  const name = results.title // pega o title que tem no objeto encontrato pelo ID
+  const salePrice = results.price // aqui pega o precço 
+  const carrinho = createCartItemElement({ sku, name, salePrice }) // função safada que retorna feião, pq era melhor o nome primeiro. 
+  // console.log(carrinho)
+  document.querySelector('.cart__items').appendChild(carrinho); // ai 'apenda' o item no carrinho, que é como se adiciona.
 }
 
 function createProductItemElement({ id: sku, title: name, thumbnail: image }) {
 // as variáveis sku, no código fornecido, se referem aos campos id retornados pela API. o tile e thumbnail eu fiz com a ajuda dos colegas - não testado ainda.
   const section = document.createElement('section');
   section.className = 'item';
-
   section.appendChild(createCustomElement('span', 'item__sku', sku));
   section.appendChild(createCustomElement('span', 'item__title', name));
   section.appendChild(createProductImageElement(image));
-  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
-  
+  const botao = createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'); // cria o botao
+// o botao tem que ter um escutador de evento - que vai receber uma função para poder receber a outra funcao, pq na 1 funcao ele não aceita o parametro, então tem que enganar ele com a arrow. 
+// o parametro a ser passado tem que ser SKU.
+  botao.addEventListener('click', (() => addCarrinho(sku)));
+  section.appendChild(botao);
   return section;
 }
 
@@ -43,24 +64,25 @@ function getSkuFromProductItem(item) {
 }
 
 function cartItemClickListener(event) {
-  event.remove(); // aqui só tem que remover tudo, limpar.
 }
 
 function createCartItemElement({ sku, name, salePrice }) {
-  const li = document.createElement('li');
-  li.className = 'cart__item';
-  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
-  return li;
+    const li = document.createElement('li'); 
+    li.className = 'cart__item';
+    li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+    li.addEventListener('click', cartItemClickListener);
+
+    return li;
 }
 
-window.onload = () => { };
-// faz o try / catch da API - pra ver se retorna mesmo os computadores 
+window.onload = () => { 
+  
+};
 window.onload = async () => {
-  try {
+  try { // faz o try / catch da API - pra ver se retorna mesmo os computadores 
     const computer = await getComputer();
-    addProdutsToScreen(computer);
   } catch (e) {
     console.log(e);
   }
+  
 };
