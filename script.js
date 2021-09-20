e0f9722adb2dc55e15a13897e43b0efd2d81c903
JoaoVMont <joaovmt1905@gmail.com>
2021-09-20 16:05:20 -0300

    Requisitos 1,2 e 3

--- script.js
+++ script.js
@@ -5,6 +5,13 @@ function createProductImageElement(imageSource) {
   return img;
 }
 
+async function getProductList() {
+  const fetchUrl = await fetch('https://api.mercadolibre.com/sites/MLB/search?q=$"computador"')
+    .then((response) => response.json());
+  const result = await fetchUrl.results;
+  return result;
+}
+
 function createCustomElement(element, className, innerText) {
   const e = document.createElement(element);
   e.className = className;
@@ -30,6 +37,8 @@ function getSkuFromProductItem(item) {
 
 function cartItemClickListener(event) {
   // coloque seu código aqui
+  const itemClicked = event.target;
+  document.addEventListener('click', itemClicked.remove())
 }
 
 function createCartItemElement({ sku, name, salePrice }) {
@@ -40,4 +49,42 @@ function createCartItemElement({ sku, name, salePrice }) {
   return li;
 }
 
-window.onload = () => { };
+async function getProductId(element) {
+  const item = element.parentElement;
+  return item.firstElementChild.innerText;
+}
+
+async function cartItems(event) {
+    // consultei o repositorio da Gabrielle Murat pra pegar algumas lógicas pro requisito 2
+  // https://github.com/tryber/sd-015-b-project-shopping-cart/tree/gabrielle-murat-shopping-cart
+  const button = event.target;
+  const productId = await getProductId(button);
+  const fetchItem = await fetch(`https://api.mercadolibre.com/items/${productId}`);
+  const data = await fetchItem.json();
+  
+  const cartList = document.querySelector('.cart__items');
+  cartList.appendChild(createCartItemElement(
+    { sku: data.id, name: data.title, salePrice: data.price },
+));
+}
+
+async function itemsListEventAdd() {
+  const itemAdd = document.querySelectorAll('.item__add');
+  itemAdd.forEach((button) => button.addEventListener('click', cartItems));
+}
+
+async function createProducts() {
+  const productList = await getProductList();
+  const sectionItems = document.querySelector('section.items');
+
+  productList.forEach((product) => {
+    const { id, title, thumbnail } = product;
+    const products = createProductItemElement({ sku: id, name: title, image: thumbnail });
+    sectionItems.appendChild(products);
+  });
+  await itemsListEventAdd();
+}
+
+window.onload = async () => {
+  createProducts();
+ };
