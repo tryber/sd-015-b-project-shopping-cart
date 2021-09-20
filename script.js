917a2c906ca200d8615e3f8e1494b4f47321eead
Wander Luiz <wander.srv@gmail.com>
2021-09-20 14:36:34 -0300

    Resoluçao dos requisitos 1, 2, 3 e 6

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const urlApi = ('https://api.mercadolibre.com/sites/MLB/search?q=$computador');
+
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
@@ -30,14 +32,73 @@ function getSkuFromProductItem(item) {
 
 function cartItemClickListener(event) {
   // coloque seu código aqui
+  event.target.remove();
+}
+
+function limparCart() {
+  const ol = document.createElement('ol');
+  const olCart = document.querySelector('.cart__items');
+  const section = document.querySelector('.cart');
+  
+  olCart.remove();
+  
+  ol.classList.add('cart__items');
+  section.append(ol); 
 }
 
 function createCartItemElement({ sku, name, salePrice }) {
   const li = document.createElement('li');
-  li.className = 'cart__item';
+  li.className = 'cart__items';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
   li.addEventListener('click', cartItemClickListener);
   return li;
 }
 
-window.onload = () => { };
+async function addItem(element) {
+  const idProduto = getSkuFromProductItem(element);
+  const itemApi = `https://api.mercadolibre.com/items/${idProduto}`;
+  
+  return fetch(itemApi)
+    .then((response) => response.json())
+    .then(({ id, title, price }) => {
+      const carList = {
+        sku: id, 
+        name: title,
+        salePrice: price,
+      };
+      const carItem = document.querySelector('.cart__items');
+      carItem.appendChild(createCartItemElement(carList));
+    });
+}
+
+function criarEventButton() {
+  const section = document.querySelectorAll('.item');
+
+  section.forEach((carItem) => carItem.lastChild
+    .addEventListener('click', (() => {
+      addItem(carItem);
+    })));
+}
+
+async function getUrl(url) {
+  return fetch(url)
+    .then((getJson) => getJson.json())
+    .then((getSearch) => getSearch.results
+      .forEach(({ id, title, thumbnail }) => {
+        const items = {
+          sku: id,
+          name: title,
+          image: thumbnail,
+        };
+      const selecionaItem = document.querySelector('.items');
+      const criaItem = createProductItemElement(items);
+      selecionaItem.append(criaItem);
+    }));
+}
+
+window.onload = () => { 
+  getUrl(urlApi)
+    .then(() => criarEventButton());
+    const botao = document.querySelector('.empty-cart');
+    botao.addEventListener('click', limparCart);
+};
