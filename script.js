acf8c6752c69fb7f3d33eb0acf900d9903bc4072
Marina Marques <marina.marqueso@outlook.com>
2021-09-20 16:10:23 -0300

    Adiciona requisito 1, reformulado apos mentoria com Carol

--- script.js
+++ script.js
@@ -1,27 +1,3 @@
-function createProductImageElement(imageSource) {
-  const img = document.createElement('img');
-  img.className = 'item__image';
-  img.src = imageSource;
-  return img;
-}
-
-// feito durante mentoria com passo a passo da carol
-// recolher e requerer api
-function getAPI() {
-  fetch("https://api.mercadolibre.com/sites/MLB/search?q=$computador")
-  .then((response) => response.json())
-  .then((obj) => createObjectProduct(obj.results))  
-}
-
-function createObjectProduct(dados) {
-  let divHTML = document.querySelector('.items');
-  //coletar o objeto da api
-  for (let key in dados) {
-    let item = createProductItemElement(dados[key]);
-    divHTML.appendChild(item);
-  }
-}
-
 function createCustomElement(element, className, innerText) {
   const e = document.createElement(element);
   e.className = className;
@@ -29,7 +5,14 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
-function createProductItemElement({ id:sku, title:name, thumbnail:image }) {
+function createProductImageElement(imageSource) {
+  const img = document.createElement('img');
+  img.className = 'item__image';
+  img.src = imageSource;
+  return img;
+}
+
+function createProductItemElement({ id: sku, title: name, thumbnail: image }) {
   const section = document.createElement('section');
   section.className = 'item';
 
@@ -41,23 +24,39 @@ function createProductItemElement({ id:sku, title:name, thumbnail:image }) {
   return section;
 }
 
-function getSkuFromProductItem(item) {
-  return item.querySelector('span.item__sku').innerText;
+function createObjectProduct(dados) {
+  const divHTML = document.querySelector('.items');
+  // coletar o objeto da api
+  const getDados = dados.forEach((element) => {
+    const item = createProductItemElement(element);
+    divHTML.appendChild(item);
+  });
+  return getDados;
+}
+// feito durante mentoria com passo a passo da carol
+// recolher e requerer api
+function getAPI() {
+  fetch('https://api.mercadolibre.com/sites/MLB/search?q=$computador')
+  .then((response) => response.json())
+  .then((obj) => createObjectProduct(obj.results));  
 }
 
-function cartItemClickListener(event) {
-  // coloque seu código aqui
-}
+// function getSkuFromProductItem(item) {
+//   return item.querySelector('span.item__sku').innerText;
+// }
 
-function createCartItemElement({ sku, name, salePrice }) {
-  const li = document.createElement('li');
-  li.className = 'cart__item';
-  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
-  return li;
-}
+// function cartItemClickListener(event) {
+//   // coloque seu código aqui
+// }
+
+// function createCartItemElement({ sku, name, salePrice }) {
+//   const li = document.createElement('li');
+//   li.className = 'cart__item';
+//   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+//   li.addEventListener('click', cartItemClickListener);
+//   return li;
+// }
 
 window.onload = () => { 
   getAPI();
-
 };
