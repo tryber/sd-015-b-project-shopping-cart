b6c2669badf32a44576c689ce393b82fa430b686
Valdênio <valdeniomartinho@gmail.com>
2021-09-20 19:11:28 -0300

    adiciona requisito 4

--- script.js
+++ script.js
@@ -44,54 +44,74 @@ function createCartItemElement({ sku, name, salePrice }) {
 
 const API_URL = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
 
-   function getProduct() {
-     return fetch(API_URL)
-     .then((data) => data.json())
-     .then((listItens) => listItens.results.forEach(({ id, title, thumbnail }) => { 
-       const listInfos = { sku: id, name: title, image: thumbnail };
-       const classItens = document.querySelector('.items');
-       const createElement = createProductItemElement(listInfos);
-       classItens.appendChild(createElement);
-     }));
-    }
-   
-    function fetchID(idItem) {
-      const valueItem = getSkuFromProductItem(idItem);
-      const API_ID = `https://api.mercadolibre.com/items/${valueItem}`;
-      fetch(API_ID)
-      .then((response) => response.json())
-      .then(({ id, title, price }) => {
-        const priceItem = {
-          sku: id,
-          name: title,
-          salePrice: price,
-        }; 
-        const cartItem = createCartItemElement(priceItem); 
-        const ol = document.querySelector('.cart__items');
-        ol.appendChild(cartItem);   
-      })
-      .catch(() => console.error('Seu item não está disponivel'));
-    }
-     
-     function botao() {
-     const items = document.querySelectorAll('.item');
-     items.forEach((item) => item.lastChild.addEventListener('click', () => fetchID(item)));
+function getProduct() {
+  return fetch(API_URL)
+    .then((data) => data.json())
+    .then((listItens) => listItens.results.forEach(({ id, title, thumbnail }) => {
+      const listInfos = { sku: id, name: title, image: thumbnail };
+      const classItens = document.querySelector('.items');
+      const createElement = createProductItemElement(listInfos);
+      classItens.appendChild(createElement);
+    }));
 }
-     /* const setItemLocal = () => {
-       const saveStorage = document.querySelector('.cart__Item').innerHTML;      
-       localStorage.setItem('chave', JSON.stringify(saveStorage)); const arrSave = [];
 
-     }; */    
+const setItemLocal = () => {
+  const saveStorage = [...document.querySelectorAll('.cart__item')];
+  console.log(saveStorage);
+  const arrSave = [];
+  saveStorage.forEach((element) => {
+    arrSave.push(element.innerHTML);
+  }); 
 
-      function apagaTudo() {
-      const listaCompleta = document.querySelector('.cart__items');
-      listaCompleta.innerHTML = ' ';
-     }     
+  localStorage.setItem('chave', JSON.stringify(arrSave));
+};
 
-window.onload = () => { 
+const getItemLocal = () => {
+  const restore = JSON.parse(localStorage.getItem('chave'));
+  restore.forEach((element) => {
+    const li = document.createElement('li');
+    const listaTotal = document.querySelector('.cart__items');
+    li.innerHTML = element;
+    li.classList.add('cart__item');
+    li.addEventListener('click', cartItemClickListener);
+    listaTotal.appendChild(li);
+  });
+};
+
+function fetchID(idItem) {
+  const valueItem = getSkuFromProductItem(idItem);
+  const API_ID = `https://api.mercadolibre.com/items/${valueItem}`;
+  fetch(API_ID)
+    .then((response) => response.json())
+    .then(({ id, title, price }) => {
+      const priceItem = {
+        sku: id,
+        name: title,
+        salePrice: price,
+      };
+      const cartItem = createCartItemElement(priceItem);
+      const ol = document.querySelector('.cart__items');
+      ol.appendChild(cartItem);
+      setItemLocal();
+    })
+    .catch(() => console.error('Seu item não está disponivel'));
+}
+
+function botao() {
+  const items = document.querySelectorAll('.item');
+  items.forEach((item) => item.lastChild.addEventListener('click', () => fetchID(item)));
+}
+
+function apagaTudo() {
+  const listaCompleta = document.querySelector('.cart__items');
+  listaCompleta.innerHTML = ' ';
+}
+
+const contentLocalStorage = localStorage.getItem('chave');
+window.onload = () => {
   getProduct()
-  .then(() => botao());
+  .then(() => botao());  
+  if (contentLocalStorage) return getItemLocal();
   const limparCarrinho = document.querySelector('.empty-cart');
   limparCarrinho.addEventListener('click', apagaTudo);
-  console.log(limparCarrinho);
 };
