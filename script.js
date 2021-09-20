d588b960b0a116befae3a577ddfda217916fbb51
Gabrielle-Murat <belle.murat@gmail.com>
2021-09-20 16:59:34 -0300

    Adiciona o requisito 6

--- script.js
+++ script.js
@@ -38,22 +38,31 @@ function createCartItemElement({ sku, name, salePrice }) {
   return li;
 }
 
-function getProductId(element) {
-  const sectionItem = element.parentElement;
-  return sectionItem.firstElementChild.innerText;
+// function getProductId(element) {
+//   const sectionItem = element.parentElement;
+//   return sectionItem.firstElementChild.innerText;
+// }
+
+function getSkuFromProductItem(item) {
+  return item.querySelector('span.item__sku').innerText;
 }
 
-async function getProductByIdFromEndpoint(event) {
-  const button = event.target;
-  const itemId = await getProductId(button);
-
-  const response = await fetch(`https://api.mercadolibre.com/items/${itemId}`);
-  const data = await response.json();
-
+function addItemToCart(data) {
   const cartItem = document.querySelector('.cart__items');
   cartItem.appendChild(createCartItemElement(
     { sku: data.id, name: data.title, salePrice: data.price },
   ));
+}
+
+async function getProductByIdFromEndpoint(event) {
+  const button = event.target;
+  const itemId = getSkuFromProductItem(button.parentElement);
+  // const itemId = await getProductId(button);
+
+  const response = await fetch(`https://api.mercadolibre.com/items/${itemId}`);
+  const data = await response.json();
+
+  addItemToCart(data);
 }
 
 async function handleItemsListButtonEventAdd() {
@@ -61,6 +70,18 @@ async function handleItemsListButtonEventAdd() {
   itemsListButtons.forEach((button) => {
     button.addEventListener('click', getProductByIdFromEndpoint);
   });
+}
+
+function handleCartListErasing() {
+  const cart = document.querySelector('.cart__items');
+  while (cart.firstChild) {
+    cart.removeChild(cart.firstChild);
+  }
+}
+
+async function eraseButtonListener() {
+  const eraseButton = document.querySelector('.empty-cart');
+  eraseButton.addEventListener('click', handleCartListErasing);
 }
 
 async function getProductsFromEndpoint() {
@@ -73,10 +94,52 @@ async function getProductsFromEndpoint() {
     ));
   });
   await handleItemsListButtonEventAdd();
+  await eraseButtonListener();
 }
 
-// function getSkuFromProductItem(item) {
-//   return item.querySelector('span.item__sku').innerText;
+// function handleCartListSaving() {
+//   const cart = document.querySelectorAll('.cart__item');
+//   const itemsSKUs = [];
+//   const itemsNames = [];
+//   const itemsSalePrices = [];
+//   cart.forEach((item) => {
+//     const itemsText = item.innerText;
+//     const itemsData = itemsText.split('|');
+//     const sku = itemsData[0].split(' ')[1];
+//     const name = itemsData[1].slice(7, itemsData[1].length - 1);
+//     const salePrice = itemsData[2].slice(9);
+    
+//     itemsSKUs.push(sku);
+//     itemsNames.push(name);
+//     itemsSalePrices.push(salePrice);
+//   });
+//   localStorage.setItem('SKUs', JSON.stringify(itemsSKUs));
+//   localStorage.setItem('Names', JSON.stringify(itemsNames));
+//   localStorage.setItem('SalePrices', JSON.stringify(itemsSalePrices));
+// }
+
+// function handleCartListRetrieving() {
+//   if (localStorage.length > 0) {
+//     handleCartListErasing();
+//     const newItemsSKUs = JSON.parse(localStorage.getItem('SKUs'));
+//     const newItemsNames = JSON.parse(localStorage.getItem('Names'));
+//     const newItemsSalePrices = JSON.parse(localStorage.getItem('SalePrices'));
+//     const listSize = newItemsSKUs.length;
+//     for (let index = 0; index < listSize; index += 1) {
+//       const cartItem = document.querySelector('.cart__items');
+//       cartItem.appendChild(createCartItemElement(
+//           { sku: newItemsSKUs[index], 
+//             name: newItemsNames[index], 
+//             salePrice: newItemsSalePrices[index] },
+//         ));
+//     }
+//   } else {
+//     return window.onload;
+//   }
+// }
+
+// function handlePageReload() {
+//   window.onreset = 
 // }
 
 // function handleAll() {
