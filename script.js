5074f358935a95785fcc202f2e4eb50e6aea1434
Rodolpho Coller <rodolpho.coller12@gmail.com>
2021-09-20 16:30:43 -0300

    adiciona requisito 05

--- script.js
+++ script.js
@@ -5,16 +5,26 @@ function createProductImageElement(imageSource) {
   return img;
 }
 
-function cartItemClickListener(event) {
+function cartItemClickListener(event, price) {
   const clicked = event.target;
+  const p = document.querySelector('.total-price');
+  p.innerText = parseFloat(p.innerText) - price;
   clicked.remove();
+}
+
+function totalPrice(value) {
+  const p = document.querySelector('.total-price');
+  let momentPrice = parseFloat(p.innerText);
+  momentPrice += value;
+  p.innerText = momentPrice;
 }
 
 function createCartItemElement({ sku, name, salePrice }) {
   const li = document.createElement('li');
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
+  console.log(salePrice);
+  li.addEventListener('click', (event) => cartItemClickListener(event, salePrice));
   return li;
 }
 
@@ -22,9 +32,11 @@ function addCartItem(id) {
   fetch(`https://api.mercadolibre.com/items/${id}`)
   .then((response) => response.json())
   .then((data) => {
+    console.log(data);
     const { id: sku, title: name, price: salePrice } = data;
-    const ol = document.querySelector(".cart__items");
+    const ol = document.querySelector('.cart__items');
     ol.appendChild(createCartItemElement({ sku, name, salePrice }));
+    totalPrice(salePrice);
   });
 }
 function createCustomElement(element, className, innerText) {
@@ -51,12 +63,12 @@ function createProductItemElement({ sku, name, image }) {
   return section;
 }
 
-function getSkuFromProductItem(item) {
-  return item.querySelector('span.item__sku').innerText;
-}
+// function getSkuFromProductItem(item) {
+//   return item.querySelector('span.item__sku').innerText;
+// }
 
 function getProducts() {
-  fetch("https://api.mercadolibre.com/sites/MLB/search?q=computador")
+  fetch('https://api.mercadolibre.com/sites/MLB/search?q=computador')
   .then((response) => response.json())
   .then((data) => data.results.forEach((computer) => {
     const { id: sku, title: name, thumbnail: image } = computer;
