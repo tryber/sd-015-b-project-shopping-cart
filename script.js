7f3d2ff80131d31a0b8a1ff9192bbf117f999f1a
Artur Henrique de Oliveira <artur.henrique45@hotmail.com>
2021-09-20 16:17:35 -0300

    primeiro requisito

--- script.js
+++ script.js
@@ -15,17 +15,13 @@ function createCustomElement(element, className, innerText) {
 function createProductItemElement({ sku, name, image }) {
   const section = document.createElement('section');
   section.className = 'item';
-
+  
   section.appendChild(createCustomElement('span', 'item__sku', sku));
   section.appendChild(createCustomElement('span', 'item__title', name));
   section.appendChild(createProductImageElement(image));
   section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
-
+  
   return section;
-}
-
-function getSkuFromProductItem(item) {
-  return item.querySelector('span.item__sku').innerText;
 }
 
 function cartItemClickListener(event) {
@@ -40,5 +36,24 @@ function createCartItemElement({ sku, name, salePrice }) {
   return li;
 }
 
-window.onload = () => { };
-// 
+const requestProducts = () => {
+  fetch('https://api.mercadolibre.com/sites/MLB/search?q=computador')
+  .then((response) => response.json())
+  .then((data) => data.results.forEach(({ id, title, thumbnail }) => {
+    const itemObject = {
+      sku: id,
+      name: title,
+      image: thumbnail,
+    };
+    const itemElement = createProductItemElement(itemObject);
+    const getClassItems = document.querySelector('.items');
+    getClassItems.appendChild(itemElement);
+  }));
+}; // ref.: https://github.com/tryber/sd-015-b-project-shopping-cart/pull/33/commits/6974a393bc7f5f84e2300355ead2200545238289
+
+function getSkuFromProductItem(item) {
+  return item.querySelector('span.item__sku').innerText;
+}
+window.onload = () => {  
+  requestProducts();
+};
