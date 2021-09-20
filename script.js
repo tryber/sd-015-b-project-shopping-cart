fed69c10231ee2aab14fe1df1b9a6872fe220f38
Valdênio <valdeniomartinho@gmail.com>
2021-09-20 19:14:08 -0300

    corrige erro de lint requisito 4

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/* eslint-disable sonarjs/no-duplicate-string */
 /* eslint-disable sonarjs/no-use-of-empty-return-value */
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
