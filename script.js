e05215a408de07304ca9d58827662c93f7addd31
Matheus Pessoa <matheuspessoafonseca@gmail.com>
2021-09-21 15:40:17 -0300

    [Requisito 2] - Adiciona o produto ao carrinho de compras. Adiciona um eventListener para todos os botoes de adicionar ao carrinho, que, ao ser clicado, adiciona o id, nome e preço do item ao carrinho

--- script.js
+++ script.js
@@ -24,6 +24,11 @@ async function fetchAPI(query = 'computador') {
   results.forEach((element) => appendProduct(element));
 }
 
+function addButtonListener() {
+  const arrayOfButtons = document.querySelectorAll('.item__add');
+  arrayOfButtons.forEach((button) => button.addEventListener('click', addItemToCartClickListener));
+}
+
 function createProductItemElement({ sku, name, image }) {
   const section = document.createElement('section');
   section.className = 'item';
@@ -33,11 +38,28 @@ function createProductItemElement({ sku, name, image }) {
   section.appendChild(createProductImageElement(image));
   section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
 
+  addButtonListener();
+
   return section;
 }
 
 function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
+}
+
+function getId(button) {
+  return getSkuFromProductItem(button.parentElement);
+}
+
+async function addItemToCartClickListener(event) {
+  const id = getId(event.target);
+  const ENDPOINT = `https://api.mercadolibre.com/items/${id}`;
+
+  const responseJSON = await fetch(ENDPOINT).then((response) => response.json());
+  const { id: sku, title: name, price: salePrice } = responseJSON;
+
+  const cart = document.querySelector('.cart__items');
+  cart.appendChild(createCartItemElement({ sku, name, salePrice }));
 }
 
 function cartItemClickListener(event) {
