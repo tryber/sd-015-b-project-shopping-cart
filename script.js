704fd9633b61050d6bb5aa873d1331b6a4402cfc
Artur Henrique de Oliveira <artur.henrique45@hotmail.com>
2021-09-20 21:05:20 -0300

    terceiro requisito

--- script.js
+++ script.js
@@ -41,16 +41,12 @@ function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
 }
   
-  function cartItemClickListener(event) {
-    
-  }
-  
   function createCartItemElement({ id: sku, title: name, price: salePrice }) {
     const li = document.createElement('li');
 
     li.className = 'cart__item';
     li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-    li.addEventListener('click', cartItemClickListener);
+    li.addEventListener('click', (event) => event.target.remove());
     return li;
   }
 
