26cb163657c66749006f2f0b19a88a40a1b5cfc4
Lucas Peres <lucasperesss@gmail.com>
2021-09-21 11:26:53 -0300

    Requisito 3 mais simples

--- script.js
+++ script.js
@@ -29,7 +29,7 @@ function getSkuFromProductItem(item) {
 }
 
 function cartItemClickListener(event) {
-  event.target.parentElement.removeChild(event.target);
+  event.target.remove();
 }
 
 function createCartItemElement({ id: sku, title: name, price: salePrice }) {
