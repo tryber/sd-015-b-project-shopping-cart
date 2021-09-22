a5d7b6091981f0b8eb356ddc659aca72db992df3
Antonio Carlos Nunes da Silva Junior <tonhaun@yahoo.com.br>
2021-09-22 18:02:25 -0300

    Cumprido o requisito 4, salvar em e carregar de localStorage

--- script.js
+++ script.js
@@ -3,12 +3,23 @@ function createProductImageElement(imageSource) {
   img.className = 'item__image';
   img.src = imageSource;
   return img;
-}    
+}
+
+const saveCartToLocalStorage = () => {
+  const currentCart = document.querySelectorAll('.cart__item');
+  const itemsToSend = [];
+  currentCart.forEach((item) => {
+    itemsToSend.push(item.innerHTML);
+  });
+  localStorage.setItem('products', JSON.stringify(itemsToSend));
+};
 
 function cartItemClickListener(event) {
   const itemToRemove = event.target;
   itemToRemove.parentElement.removeChild(itemToRemove);
-}  
+  saveCartToLocalStorage();
+  // Fazer com que o objeto removido da OL também seja removido no localStorage
+}    
 
 function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
@@ -20,7 +31,7 @@ function createCartItemElement({ sku, name, salePrice }) {
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
   li.addEventListener('click', cartItemClickListener);
   return li;
-}
+}  
 
 const requestToAddOnCart = async (itemId) => {
 try {
@@ -32,10 +43,12 @@ try {
     const computerToAdd = createCartItemElement({ sku, name, salePrice });
     const listToAddTheComputerOn = document.querySelector('.cart__items');
     listToAddTheComputerOn.appendChild(computerToAdd);
+    // sendCartToLocalStorage({ sku, name, salePrice });
+    saveCartToLocalStorage();
   } catch (error) {
     console.log(error);
   }  
-};  
+};
 
 const getProductId = (event) => {
   const selectedProductID = getSkuFromProductItem(event.target.parentElement);
@@ -80,9 +93,35 @@ const requestProducts = async () => {
     });    
   } catch (error) {
     console.log(error);
-  }    
+  }
 };    
+
+const loadCartFromLocalStorage = () => {
+  const cartLoaded = JSON.parse(localStorage.getItem('products'));
+  const containerToFill = document.querySelector('.cart__items');
+  if (!cartLoaded) {
+    return;
+  }
+  cartLoaded.forEach((item) => {
+    const itemToCart = createCustomElement('li', 'cart__item', item);
+    itemToCart.addEventListener('click', cartItemClickListener);
+    containerToFill.appendChild(itemToCart);
+  });
+};
+
+/**
+ * Para entender e resolver o requisito 4 eu precisei consultar as requisições para puxar dos colegas:
+ * Amanda Fernandes, em https://github.com/tryber/sd-015-b-project-shopping-cart/pull/63/commits/d951498d3ba358eb447c2943510230d9c40c8b36 e 
+ *  https://github.com/tryber/sd-015-b-project-shopping-cart/pull/63/commits/5e632329c8ba06d09377a60fa9b2433d4485684b
+ * Gabriel Benedicto: https://github.com/tryber/sd-015-b-project-shopping-cart/pull/96/commits/10e164a7de6b10b84a373a59e301ce0e23048e70
+ * Daniel Hott: https://github.com/tryber/sd-015-b-project-shopping-cart/pull/87/commits/838e21e46099c7713194fd8f03ff82b3e5ad52c7
+ * Antonio Campos: https://github.com/tryber/sd-015-b-project-shopping-cart/pull/28/commits/c354ccbd5b388654e1d1176f81b587c79eb75002
+ * É, eu rachei a cuca e os colegas me ajudaram sem saber. Gostaria de ter tirado as dúvidas numa chamada, numa mentoria ou pelo slack.
+ * Mas nem tive tempo durante a semana para acessar esses canais. O importante pra mim foi ter conseguido desenvolver uma lógica e uma dinâmica
+ * para resolver a questão.
+ */
 
 window.onload = () => {
   requestProducts();
+  loadCartFromLocalStorage();
 };
