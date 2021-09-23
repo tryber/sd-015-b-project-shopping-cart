9eae16faac0239371e7e762dc15f99c2a253a1b1
Joao Pedro de Oli. Lage <JoaoPedro.Lage@outlook.com>
2021-09-23 16:47:05 -0300

    Implementa lógica para add produto(s) ao carrinho

--- script.js
+++ script.js
@@ -40,17 +40,45 @@ function createCartItemElement({ sku, name, salePrice }) {
   return li;
 }
 
+async function addProductToCart(event) {
+  const itemId = event.target.parentNode.firstChild.innerText;
+  const responseiTem = await fetch(`https://api.mercadolibre.com/items/${itemId}`);
+  const dataItem = await responseiTem.json();
+
+  const olHTML = document.querySelector('.cart__items');
+  const result = { sku: '', name: '', salePrice: '' };
+
+  result.sku = dataItem.id; result.name = dataItem.title; result.salePrice = dataItem.price;
+  return olHTML.appendChild(createCartItemElement(result));
+}
+
+function buttonEventListener() {
+  const addToCartButton = document.querySelectorAll('.item__add');
+  for (let i = 0; i < addToCartButton.length; i += 1) {
+    addToCartButton[i].addEventListener('click', addProductToCart);
+  }
+  // addToCartButton.forEach((_, i) => addToCartButton[i].addEventListener('click', addProductToCart));
+}
+
+// async function getImgOfProducts(img) {
+//   const responseImg = await fetch(`https://api.mercadolibre.com/items/${img}`);
+//   const dataImg = await responseImg.json();
+
+//   return dataImg.pictures[0].url;
+// }
 async function getProducts() {
   const response = await fetch('https://api.mercadolibre.com/sites/MLB/search?q=computador');
   const data = await response.json();
-
   const divHTMl = document.querySelector('.items');
 
-  return (data.results).map((productObj) => {
+  data.results.forEach((productObj) => {
     const result = { sku: '', name: '', image: '' };
-    result.sku = productObj.id; result.name = productObj.title; result.image = productObj.thumbnail;
-    return divHTMl.appendChild(createProductItemElement(result));
+    result.sku = productObj.id; result.name = productObj.title;
+    result.image = productObj.thumbnail;
+    const createProducts = divHTMl.appendChild(createProductItemElement(result));
+    return createProducts;
   });
+  buttonEventListener();
 }
 
 window.onload = async () => {
