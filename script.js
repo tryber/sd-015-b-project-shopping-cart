e87c6b425ec34da53256c1e13f3e71adfce07d78
DanMondaini <danielmondaini@outlook.com>
2021-09-23 15:14:12 -0300

    req 1

--- script.js
+++ script.js
@@ -40,4 +40,34 @@ function createCartItemElement({ sku, name, salePrice }) {
   return li;
 }
 
-window.onload = () => { };
+// Consultei o Erickson Siqueira para resolução desse requisito - link rep: https://github.com/tryber/sd-015-b-project-shopping-cart/pull/44/commits/e0863c5719b2f71b4ba9d503f045f084a9270797
+
+async function getProductList(productName) {
+  const API_URL = `https://api.mercadolibre.com/sites/MLB/search?q=${productName}`;
+  const productListPromise = await fetch(API_URL);
+  const productListConverted = await productListPromise.json();
+
+  return productListConverted;
+}
+
+function makeObjForProductItem(product) {
+  const { id, title, thumbnail } = product;
+  const productObj = { sku: id, name: title, image: thumbnail };
+  return productObj;
+}
+
+async function generateProductList(prodctName) {
+  const productList = await getProductList(prodctName);
+  const prodctListResults = productList.results;
+  const itemsSections = document.querySelector('.items');
+
+  prodctListResults.forEach((product) => {
+    const objForProductItem = makeObjForProductItem(product);
+    const productItemElement = createProductItemElement(objForProductItem);
+    itemsSections.appendChild(productItemElement);
+  });
+}
+
+window.onload = () => {
+  generateProductList('computador');
+};
