1dd62cd2f3be9b6d6cc96f44eb4b4d30fcbbb730
Toineto <augusto.vilhena4@gmail.com>
2021-09-24 16:52:51 -0300

    adiciona requisito 4

--- script.js
+++ script.js
@@ -36,13 +36,18 @@ function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
 }
 
+function saveItems() {
+  const cart = document.querySelector('.cart');
+  localStorage.setItem('shoppingCart', cart.innerHTML);
+}
+
 function cartItemClickListener(event) {
   const item = event.target;
-  item.parentElement.removeChild(item);  
+  item.parentElement.removeChild(item);
+  saveItems();
 }
 
 function createCartItemElement({ id: sku, title: name, price: salePrice }) {
-  console.log(sku);
   const li = document.createElement('li');
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
@@ -62,7 +67,8 @@ function itemClickListener(event) {
   const itemID = section.children[0].innerText;
   fetch(`https://api.mercadolibre.com/items/${itemID}`)
     .then((item) => item.json())
-    .then((item) => handleCartItemElement(item));
+    .then((item) => handleCartItemElement(item))
+    .then(() => saveItems());
 }
 
 function handleAddBtn() {
@@ -78,6 +84,17 @@ function requestProductAsync() {
     .then(() => handleAddBtn());
 }
 
+function loadItems() {
+  const cart = document.querySelector('.cart');
+  const savedCart = localStorage.getItem('shoppingCart');
+  if (savedCart) {
+    cart.innerHTML = savedCart;
+  }
+  const itemList = document.querySelectorAll('.cart__item');
+  itemList.forEach((item) => item.addEventListener('click', cartItemClickListener));
+}
+
 window.onload = () => {
   requestProductAsync();
+  loadItems();
 };
