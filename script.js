2242e099464a0f25e675e0adf4f418116214d680
uroque <ulissesroque@protonmail.com>
2021-09-24 16:22:18 -0300

    Novo push apos clonar o repositorio novamente

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const ML_API_URL = 'https://api.mercadolibre.com';
+
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
@@ -12,24 +14,13 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
-function createProductItemElement({ sku, name, image }) {
-  const section = document.createElement('section');
-  section.className = 'item';
-
-  section.appendChild(createCustomElement('span', 'item__sku', sku));
-  section.appendChild(createCustomElement('span', 'item__title', name));
-  section.appendChild(createProductImageElement(image));
-  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
-
-  return section;
-}
-
-function getSkuFromProductItem(item) {
-  return item.querySelector('span.item__sku').innerText;
-}
+// Busca o 'sku' de um item
+// function getSkuFromProductItem(item) {
+//   return item.querySelector('span.item__sku').innerText;
+// }
 
 function cartItemClickListener(event) {
-  // coloque seu código aqui
+  event.target.remove();
 }
 
 function createCartItemElement({ sku, name, salePrice }) {
@@ -40,4 +31,67 @@ function createCartItemElement({ sku, name, salePrice }) {
   return li;
 }
 
-window.onload = () => { };
+// Adiciona itens ao carrinho
+async function addItemToCart(sku) {
+  const loading = document.createElement('section');
+  loading.className = 'loading';
+  loading.innerText = 'loading...';
+  document.querySelector('.items').appendChild(loading);
+  
+  const requestItem = await fetch(`${ML_API_URL}/items/${sku}`);
+  const receivedItem = await requestItem.json();
+  const { title, price } = receivedItem;
+  const itemObj = {
+    sku,
+    name: title,
+    salePrice: price,
+  };
+  
+  document.querySelector('.cart__items').appendChild(createCartItemElement(itemObj));
+}
+
+function createProductItemElement({ sku, name, image }) {
+  const section = document.createElement('section');
+  section.className = 'item';
+
+  section.appendChild(createCustomElement('span', 'item__sku', sku));
+  section.appendChild(createCustomElement('span', 'item__title', name));
+  section.appendChild(createProductImageElement(image));
+  const addToCartButton = createCustomElement('button', 'item__add', 'Adicionar ao carrinho!');
+  // Cria event listeners para os botões de 'Adicionar ao carrinho!'
+  addToCartButton.addEventListener('click', () => {
+    addItemToCart(sku);
+  });
+  section.appendChild(addToCartButton);   
+  return section;
+}
+
+// Faz requerimento de busca à API
+async function requestSearchToML() {
+  const product = 'computador';
+  const loading = document.createElement('section');
+  loading.className = 'loading';
+  loading.innerText = 'loading...';
+  document.querySelector('.items').appendChild(loading);
+  const requestProductSearch = await fetch(`${ML_API_URL}/sites/MLB/search?q=${product}`); // envia requerimento à API
+  const productList = await requestProductSearch.json(); // faz um parse dos resultados entregues pela API para o formato .json
+  const productListResults = productList.results; // cria array com os resultados (especificamente o parâmetro .results) 
+  document.querySelector('.loading').remove();
+  productListResults.forEach((_product) => {
+    const sku = _product.id;
+    const name = _product.title;
+    const image = _product.thumbnail;
+    const itemsClass = document.querySelector('.items');
+    itemsClass.appendChild(createProductItemElement({ sku, name, image }));
+  });
+}
+
+// Cria event listener no botão 'Esvaziar carrinho', que remove todos os itens
+document.querySelector('.empty-cart').addEventListener('click', () => {
+  document.querySelectorAll('.cart__item').forEach((item) => item.remove());
+});
+
+window.onload = () => {
+  // Envia a requisição à API
+  requestSearchToML();
+};
