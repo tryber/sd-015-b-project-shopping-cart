c7ee3e331892450a9cd0c0b76ce1693f9905d370
diego.camomila@gmail.com <git@github.com:diegocamomila/repositorio--tetste.git>
2021-09-24 14:38:09 -0300

    requisitos 1 ao 3 refatorados

--- script.js
+++ script.js
@@ -4,11 +4,61 @@ function createProductImageElement(imageSource) {
   img.src = imageSource;
   return img;
 }
+// abaixo requisito 2.5 ja implantado
+function cartItemClickListener(event) {
+  // abaixo requisito 3 ja implantado
+  const cartItemOL = document.querySelector('.cart__items');
+  cartItemOL.removeChild(event.target);
+}
+
+// abaixo requisito 2.4 ja implantado
+function createCartItemElement({ sku, name, salePrice }) {
+  const li = document.createElement('li');
+  li.className = 'cart__item';
+  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+  li.addEventListener('click', cartItemClickListener);
+    return li;
+}
+
+// a baixo requisito 2.3 ja implementado
+function getSkuFromProductItem(item) {
+  return item.querySelector('span.item__sku').innerText;
+}
+
+// a baixo requisito 2.2
+const buscarApiId = (itemID) => {
+  try {
+    const apiProdutoId = `https://api.mercadolibre.com/items/${itemID}`;
+
+    return fetch(apiProdutoId)
+      .then((response) => response.json())
+      .then((object) => {
+        const produtoId = object;
+        return produtoId;
+      });
+  } catch (error) {
+  console.log('Erro, endereço não encontrado');
+}
+};
+
+// a baixo requisito 2.1
+const buscaritemId = async (event) => {
+  const clickElementoPai = event.target.parentNode;
+  const idItem = getSkuFromProductItem(clickElementoPai);
+  const objectItemID = await buscarApiId(idItem);
+  const { id: sku, title: name, price: salePrice } = objectItemID;
+  const cartItemList = createCartItemElement({ sku, name, salePrice });
+  const cartList = document.querySelector('.cart__items');
+  cartList.appendChild(cartItemList);
+}; // rafel machado guimaraes como referencia
 
 function createCustomElement(element, className, innerText) {
   const e = document.createElement(element);
   e.className = className;
   e.innerText = innerText;
+  if (element === 'button') {
+    e.addEventListener('click', buscaritemId); // if chamada p/ o botao. requisito 2
+  }
   return e;
 }
 
@@ -24,73 +74,29 @@ function createProductItemElement({ sku, name, image }) {
   return section;
 }
 
-function cartItemClickListener(event) {
-  // coloque seu código aqui
-  const ol = event.path[1];
-  ol.removeChild(event.path[0]);
-  localStorage.setItem('click', ol.innerHTML);
-}
-
-// abaixo requisito 2.5 ja implantado
-function createCartItemElement({ sku, name, salePrice }) {
-  const li = document.createElement('li');
-  li.className = 'cart__item';
-  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
-  return li;
-}
-
-// abaixo requisito 2.4 ja implantado
-function getSkuFromProductItem(item) {
-  return item.querySelector('span.item__sku').innerText;
-}
-
-// a cima codigo  que ja implementado ---------------------------------------------
-
-// abaixo requisito 2.3 
-const addNoCarrinho = async (item) => {
-  const pegaId = getSkuFromProductItem(item);
-  fetch(`https://api.mercadolibre.com/items/${pegaId}`)
-    .then((response) => response.json())
-    .then(({ id, title, price }) => {
-      const dadosId = {
-        sku: id,
-        name: title,
-        salePrice: price,
-      };
-      const ol = document.querySelector('.cart__items');
-      ol.append(createCartItemElement(dadosId));
-    });
-};
-
-// abairo requisito 2.2
-const addBotao = () => {
-  const produtos = document.querySelectorAll('.item');
-  console.log(produtos);
-  produtos.forEach((item) => item.lastChild.addEventListener('click', (() => 
-  addNoCarrinho(item))));
-
-  return produtos;
-};
-
 // a baixo requisito 1 monitoria carlol e rafael colombo --------------
-const buscarProduto = async () => 
-  fetch('https://api.mercadolibre.com/sites/MLB/search?q=computador')
+const mercadoLivreApi = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
+
+function buscarProduto() {
+  try {
+    fetch(mercadoLivreApi)
   .then((response) => response.json())
   .then((data) => data.results.forEach(({ id, title, thumbnail }) => {
-    const filtroResultadosObj = { sku: id, name: title, image: thumbnail };
+    const filtroResultadosObj = { 
+      sku: id, 
+      name: title, 
+      image: thumbnail,
+    };
     const selecinaItem = document.querySelector('.items');
     const item = createProductItemElement(filtroResultadosObj);
     selecinaItem.append(item);
   }));
-  
-// a baixo requisito 2.1 inicio - buscaproduto veio para cá e window.onload inicia agora com loadOrden rafael colombo e virginiaVerso.------------------------------
-function gerarPedido() { 
-  buscarProduto()
-    .then(() => addBotao())
-    .catch(() => ('Erro, endereço não encontrado'));
+} catch (error) {
+  console.log('Erro, endereço não encontrado');
+}
 }
 
 window.onload = () => {
-  gerarPedido();
-};
+  // gerarPedido();
+  buscarProduto();
+  };
