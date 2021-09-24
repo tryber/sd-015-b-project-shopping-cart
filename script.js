4bb5c95665c8d763ea37d1e93f96f288894460ee
Alessandro Fuhr de Mello <ale_95_f.mello@hotmail.com>
2021-09-24 20:45:04 -0300

    Implementa a funçao addItemToCart para resolver o requisito 2

--- script.js
+++ script.js
@@ -14,18 +14,6 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
-function createProductItemElement({ sku, name, image }) {
-  const section = document.createElement('section');
-  section.className = 'item';
-
-  section.appendChild(createCustomElement('span', 'item__sku', sku));
-  section.appendChild(createCustomElement('span', 'item__title', name));
-  section.appendChild(createProductImageElement(image));
-  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
-
-  return section;
-}
-
 function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
 }
@@ -40,6 +28,37 @@ function createCartItemElement({ sku, name, salePrice }) {
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
   li.addEventListener('click', cartItemClickListener);
   return li;
+}
+
+function addItemToCart() {
+  const itemId = getSkuFromProductItem(this.parentElement);
+  const itemApiUrl = `https://api.mercadolibre.com/items/${itemId}`;
+  
+  fetch(itemApiUrl)
+    .then((response) => response.json())
+    .then(({ title, price }) => {
+      const item = document.querySelector('.cart__items');
+      const itemInfo = {
+        sku: itemId,
+        name: title,
+        salePrice: price,
+      };
+
+      item.appendChild(createCartItemElement(itemInfo));
+    });
+}
+
+function createProductItemElement({ sku, name, image }) {
+  const section = document.createElement('section');
+  section.className = 'item';
+
+  section.appendChild(createCustomElement('span', 'item__sku', sku));
+  section.appendChild(createCustomElement('span', 'item__title', name));
+  section.appendChild(createProductImageElement(image));
+  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'))
+    .addEventListener('click', addItemToCart);
+
+  return section;
 }
 
 async function verifyFetch(url) {
