75b9d39e237b07c8ddb697d3704c9c7e516b20b9
Lucas Maieski Marques <lucmaieski@gmail.com>
2021-09-25 03:36:31 -0300

    Feito requisito 5

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-let acumulador = 0;
-
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
@@ -17,12 +15,10 @@ function createCustomElement(element, className, innerText) {
 function createProductItemElement({ sku, name, image }) {
   const section = document.createElement('section');
   section.className = 'item';
-
   section.appendChild(createCustomElement('span', 'item__sku', sku));
   section.appendChild(createCustomElement('span', 'item__title', name));
   section.appendChild(createProductImageElement(image));
   section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
-
   return section;
 }
 
@@ -32,6 +28,10 @@ function getSkuFromProductItem(item) {
 
 function cartItemClickListener(event) {
   event.target.remove();
+  const span = document.querySelector('.total-price');
+  const valorSub = event.target.innerText.split('$')[1];
+  const acumulador = span.innerText - Number(valorSub);
+  span.innerHTML = acumulador;
 }
 
 function createCartItemElement({ sku, name, salePrice }) {
@@ -42,55 +42,62 @@ function createCartItemElement({ sku, name, salePrice }) {
   return li;
 }
 
-function handleButtonCallback(event) {
+function somaTotal(preço) {
   const span = document.querySelector('.total-price');
+  const acumulador = preço + Number(span.innerText);
+  span.innerText = acumulador;
+}
+
+function handleButtonCallback(event) {
   const objeto = event.target.parentNode;
   const id = getSkuFromProductItem(objeto);
   const url = `https://api.mercadolibre.com/items/${id}`;
   fetch(url)
-  .then((response) => response.json())
-  .then((item) => {
-    acumulador += item.price;
-    span.innerHTML = acumulador;
-    const ol = document.querySelector('.cart__items');
-    ol.appendChild(createCartItemElement(
-      { sku: item.id, name: item.title, salePrice: item.price },
-    ));
-  });
+    .then((response) => response.json())
+    .then((item) => {
+      console.log(item);
+      const ol = document.querySelector('.cart__items');
+      const { id: sku, title: name, price: salePrice } = item;
+      const test = createCartItemElement({ sku, name, salePrice });
+      ol.appendChild(test);
+      somaTotal(salePrice);
+    });
 }
 
 function handleButtonId() {
   const buttonId = document.querySelectorAll('.item__add');
-  buttonId.forEach((element) => element.addEventListener('click', handleButtonCallback));
-}
-
-async function getItemsFromAPI() {
-  const url = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
-  fetch(url)
-  .then((result) => result.json())
-  .then((infos) => infos.results)
-  .then((item) => item.forEach((element) => {
-    const items = document.querySelector('.items');
-    items.appendChild(createProductItemElement(
-      { sku: element.id, name: element.title, image: element.thumbnail },
-    ));
-  }))
-  .then(() => handleButtonId())
-  .then(() => {
-    const loading = document.querySelector('.loading');
-    loading.remove();
+  buttonId.forEach((element) => {
+    element.addEventListener('click', handleButtonCallback);
   });
 }
 
-function cleanList() {
-  const cleanListButton = document.querySelector('.empty-cart');
-  cleanListButton.addEventListener('click', () => {
+function getItemsFromAPI() {
+  const url = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
+  fetch(url)
+    .then((result) => result.json())
+    .then((items) => items.results)
+    .then((apenda) => apenda.forEach((element) => {
+      const item = document.querySelector('.items');
+      item.appendChild(createProductItemElement(
+        { sku: element.id, name: element.title, image: element.thumbnail },
+      ));
+    }))
+.then(() => handleButtonId())
+    .then(() => {
+      const loading = document.querySelector('.loading');
+      loading.remove();
+    });
+}
+
+function limpaCarrinho() {
+  const botaoDeEsvaziar = document.querySelector('.empty-cart');
+  botaoDeEsvaziar.addEventListener('click', () => {
     const li = document.querySelectorAll('.cart__item');
     li.forEach((element) => element.remove());
   });
 }
 
-window.onload = () => { 
+window.onload = () => {
   getItemsFromAPI();
-  cleanList();
+  limpaCarrinho();
 };
