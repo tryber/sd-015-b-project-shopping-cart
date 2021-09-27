cb578eddb44eada1b33b81b7f0bd795b01177301
Gabriel Borges <igblborges@gmail.com>
2021-09-27 11:20:55 -0300

    refatorando-requisito-2-para-passar-test

--- script.js
+++ script.js
@@ -15,10 +15,12 @@ function createCustomElement(element, className, innerText) {
 function createProductItemElement({ sku, name, image }) {
   const section = document.createElement('section');
   section.className = 'item';
+  
   section.appendChild(createCustomElement('span', 'item__sku', sku));
   section.appendChild(createCustomElement('span', 'item__title', name));
   section.appendChild(createProductImageElement(image));
-  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));  
+  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
+  
   return section;
 }
 
@@ -26,52 +28,54 @@ function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
 }
 
-function cartItemClickListener(event) {
-     event.target.remove();
-    }
+function cartItemClickListener(event) { 
+  event.target.remove(); 
+}
 
 function createCartItemElement({ sku, name, salePrice }) {
   const li = document.createElement('li');
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
   li.addEventListener('click', cartItemClickListener);
-  return li;
+  return li;  
 }
 
- const requestItem = async (item) => {
-  const responseFetch = await fetch(`https:api.mercadolibre.com/items/${item}`);  
-  const objectResponse = await responseFetch.json();  
-  const objectParameter = {
-    sku: objectResponse.id, 
-    name: objectResponse.title, 
-    salePrice: objectResponse.price,
-  };
-  const classCartItem = document.querySelector('.cart__items');
-  return classCartItem.appendChild(createCartItemElement(objectParameter));   
- };
-const getCartitem = () => {
-  const getButtonAdd = document.querySelectorAll('.item__add');  
-    getButtonAdd.forEach((button) => button.addEventListener('click', 
-    (event) => {      
-      const idComputer = (event.path[1].firstChild.innerHTML);
-      requestItem(idComputer);      
-    }));   
-  };
-const getComputador = async () => {  
-  const response = await fetch('https://api.mercadolibre.com/sites/MLB/search?q=Computador');
+// function addCartItemElement() {  
+  //   const cartButtons = document.querySelectorAll('.item__add');
+  //    cartButtons.forEach((element) => {
+    //      console.log(element);
+    //    });
+    // const getClassItem = document.getElementsByClassName('item__add');  
+    // }
+    async function getidItem(idItem) {
+    const response = await fetch(`https://api.mercadolibre.com/items/${idItem}`);
+    const objectJason = await response.json(); 
+     const { id, title, price } = objectJason;
+     const creatCartLi = createCartItemElement({ sku: id, name: title, salePrice: price });
+     const ol = document.querySelector('.cart__items');
+     ol.appendChild(creatCartLi); 
+    }   
+    function getButton() {
+  const addCartButton = document.querySelectorAll('.item__add');
+  addCartButton.forEach((button) => {
+    button.addEventListener('click', (element) => {
+      const idElement = (element.target.parentElement.firstChild.innerText); 
+      getidItem(idElement);     
+    });    
+  });
+} 
+
+async function getComputerApi() {
+  const response = await fetch('https://api.mercadolibre.com/sites/MLB/search?q=computador');
   const objectJason = await response.json();
   objectJason.results.forEach(({ id, title, thumbnail }) => {
-    const forEachElement = createProductItemElement({ 
-      sku: id,
-      name: title,
-      image: thumbnail,
-    });    
-    const sectionItem = document.querySelector('.items');
-    sectionItem.appendChild(forEachElement);
-  });  
-  getCartitem();
-};
+    const creatProducts = createProductItemElement({ sku: id, name: title, image: thumbnail });   
+    const classItem = document.querySelector('.items');
+    classItem.appendChild(creatProducts);        
+});
+getButton();
+} 
 
 window.onload = () => { 
-  getComputador();
- };
+  getComputerApi();   
+};
