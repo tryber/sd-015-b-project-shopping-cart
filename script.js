509c379490634ea646b7a91e88b68d0b8e10489e
samuel <samuelsoutosilva14@gmail.com>
2021-10-01 21:44:10 -0300

    adiciona requisito 4

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const MLBurl = 'https://api.mercadolibre.com/sites/MLB/search?q=$computador';
+const recordedDataCart = localStorage.getItem('cartActual');
 
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
@@ -14,13 +15,41 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
+function cartSave() {
+  const cartItems = [...document.querySelectorAll('.cart__item')];
+  const auxLocalStorage = [];
+
+  cartItems.forEach((item) => {
+    auxLocalStorage.push(item.innerHTML);
+  });
+
+  localStorage.setItem('cartActual', JSON.stringify(auxLocalStorage));
+}
+
 function cartItemClickListener(event) {
   event.target.remove();
+  cartSave();
+}
+
+function restoreLiCart(itemsLocal) {
+  const cartList = document.querySelector('.cart__items');
+  itemsLocal.forEach((item) => {
+    const row = document.createElement('li');
+    row.innerHTML = item;
+    row.classList.add('cart__item');
+    row.addEventListener('click', cartItemClickListener);
+    cartList.appendChild(row);
+  });
+}
+
+function restoreCartSave() {
+  const cartAnterior = JSON.parse(localStorage.getItem('cartActual'));
+  restoreLiCart(cartAnterior);
 }
 
 function createCardItemElement({ id, title, price }) {
   const row = document.createElement('li');
-  row.classList = 'cart__items';
+  row.classList = 'cart__item';
   row.innerText = `SKU: ${id} | NAME: ${title} | PRICE: $${price}`;
   row.addEventListener('click', cartItemClickListener);
   return row;
@@ -30,9 +59,10 @@ function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
 }
 
-function createList(output) {
+function createList(data) {
   const list = document.querySelector('.cart__items');
-  list.appendChild(output);
+  list.appendChild(data);
+  cartSave();
 }
 
 async function searchIdInResult(element) {
@@ -46,9 +76,6 @@ async function searchIdInResult(element) {
 
 function addListenerButton() { 
   const items = document.querySelectorAll('.item');
-  // items.forEach((item) => item.addEventListener('click', (() => {
-  //   searchIdInResult(item);
-  // })));
   Object.keys(items).forEach((item) => {
     items[item].addEventListener('click', searchIdInResult);
   });
@@ -75,8 +102,8 @@ async function getRequest(url) {
   return fetch(url)
     .then((response) => response.json())
     .then((listSearch) => listSearch.results.forEach(({ id, title, thumbnail }) => {
-      const output = { sku: id, name: title, image: thumbnail };
-      getElementsItems(output); 
+      const objCartMod = { sku: id, name: title, image: thumbnail };
+      getElementsItems(objCartMod); 
     }));
 }
 
@@ -86,5 +113,6 @@ function order() {
 }
 
 window.onload = () => {
+  if (recordedDataCart) restoreCartSave();
   order();
 };
