f47333aa9e3a98fc58424fa684d0fdb2eaa4c879
aklimak <alexandre.skk@gmail.com>
2021-10-04 06:42:21 -0300

    Adiciona requisitos 1 e 2

--- script.js
+++ script.js
@@ -5,10 +5,42 @@ function createProductImageElement(imageSource) {
   return img;
 }
 
+function cartItemClickListener(event) {
+  // coloque seu código aqui
+}
+
+function createCartItemElement({ sku, name, salePrice }) {
+  const li = document.createElement('li');
+  li.className = 'cart__item';
+  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+  li.addEventListener('click', cartItemClickListener);
+  return li;
+}
+
+function idToProduct(i) {
+  fetch(`https://api.mercadolibre.com/items/${i}`)
+  .then((response) => response.json())
+  .then(({ id, title, price }) => {
+    console.log({ id, title, price });
+    const items = document.querySelector('.cart__items');
+    items.appendChild(createCartItemElement(
+      { sku: id, name: title, salePrice: price },
+    ));
+  });
+}
+
+function getSkuFromProductItem() {
+  idToProduct(this.parentNode.querySelector('span.item__sku').innerText);
+  return this.parentNode.querySelector('span.item__sku').innerText;  
+}
+
 function createCustomElement(element, className, innerText) {
   const e = document.createElement(element);
   e.className = className;
   e.innerText = innerText;
+  if (element === 'button') {
+    e.addEventListener('click', getSkuFromProductItem);
+  } 
   return e;
 }
 
@@ -24,20 +56,18 @@ function createProductItemElement({ sku, name, image }) {
   return section;
 }
 
-function getSkuFromProductItem(item) {
-  return item.querySelector('span.item__sku').innerText;
-}
+const fetchApi = () => {
+  fetch('https://api.mercadolibre.com/sites/MLB/search?q=computer')
+  .then((response) => response.json())
+  .then((object) => object.results)
+  .then((results) => results.forEach((element) => {
+    const computers = document.querySelector('.items');
+    computers.appendChild(createProductItemElement(
+      { sku: element.id, name: element.title, image: element.thumbnail },
+      ));    
+  }));
+};
 
-function cartItemClickListener(event) {
-  // coloque seu código aqui
-}
-
-function createCartItemElement({ sku, name, salePrice }) {
-  const li = document.createElement('li');
-  li.className = 'cart__item';
-  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
-  return li;
-}
-
-window.onload = () => { };
+window.onload = () => { 
+  fetchApi();
+};
